0af15ce4f2f623d7a1a3abbd544b664b822295d8
p4535992 <tentimarco0@gmail.com>
2021-09-18 10:50:54 +0200

    Remove dist

--- dist/module/ArmsReachSocket.js
+++
@@ -1,9 +0,0 @@
-import { getCanvas } from "./settings.js";
-export let socket;
-export function recalculate(tokens) {
-    socket.executeForEveryone(_socketRecalculate, tokens ? tokens.map((token) => token.id) : undefined);
-}
-export function _socketRecalculate(tokenIds) {
-    //@ts-ignore
-    return getCanvas().controls?.ruler?.dragRulerRecalculate(tokenIds);
-}
