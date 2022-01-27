0f85423651fd4703016569b4faa2c56d733c79f7
p4535992 <tentimarco0@gmail.com>
2022-01-27 13:54:30 +0100

    better center coordinates calculation

--- src/module/ArmsReachHelper.ts
+++ src/module/ArmsReachHelper.ts
@@ -153,12 +153,50 @@ export const getTokenCenter = function (token) {
     const shape0 = shapes[0];
     return { x: shape0.x, y: shape0.y };
   }
-  const tokenCenter = { x: token.x + token.w / 2, y: token.y + token.h / 2 };
+  // const tokenCenter = { x: token.x + token.w / 2, y: token.y + token.h / 2 };
+  const tokenCenter = getCenter(token);
   return tokenCenter;
 };
 
 /**
+ * Get center
+ * from tokenAttacher module
+ */
+export const getCenter = function (placeableObject: PlaceableObject, grid:any = {}): { x: number; y: number } {
+  const data = placeableObject.data;
+  //getCenter(type, data, grid = {}){
+  let isGridSpace = false;
+  if(placeableObject.document.documentName == TileDocument.documentName) {
+    isGridSpace = false;
+  }
+  else if(placeableObject.document.documentName == DrawingDocument.documentName){
+    isGridSpace = false;
+  }
+	else 	{
+    isGridSpace = true;
+	}
+  grid = mergeObject({w: canvas.grid?.w, h:canvas.grid?.h}, grid);
+  const [x,y] = [data.x, data.y];
+  let center = {x:x, y:y};
+  //Tokens, Tiles
+  if ( "width" in data && "height" in data ) {
+    let [width, height] = [data.width, data.height];
+    if(isGridSpace){
+      [width, height] = [width * grid.w, height * grid.h];
+    }
+    center={x:x + (Math.abs(width) / 2), y:y + (Math.abs(height) / 2)};
+  }
+  //Walls
+  if("c" in data){
+    //@ts-ignore
+    center = {x:(data.c[0] + data.c[2]) / 2, y: (data.c[1] + data.c[3]) / 2}
+  }
+  return center;
+}
+
+/**
  * Get token shape center
+ * from dragRuler module
  */
 function getTokenShape(token): any[] {
   if (token.scene.data.gridType === CONST.GRID_TYPES.GRIDLESS) {
@@ -430,22 +468,32 @@ export const getPlaceablesAt = function (placeables, position): PlaceableObject[
 };
 
 export const placeableContains = function (placeable, position): boolean {
-  const x = getPlaceableX(placeable);
-  const y = getPlaceableY(placeable);
+  // const x = getPlaceableX(placeable);
+  // const y = getPlaceableY(placeable);
+
+  const center = getCenter(placeable);
+  const x = center.x;
+  const y = center.y;
+
   const w = getPlaceableWidth(placeable);
   const h = getPlaceableHeight(placeable);
   return Number.between(position.x, x, x + w) && Number.between(position.y, y, y + h);
 };
 
 export const getPlaceableCenter = function (placeable: any): any {
-  const x = getPlaceableX(placeable);
-  const y = getPlaceableY(placeable);
+  // const x = getPlaceableX(placeable);
+  // const y = getPlaceableY(placeable);
+
+  const center = getCenter(placeable);
+  const x = center.x;
+  const y = center.y;
+
   const w = getPlaceableWidth(placeable);
   const h = getPlaceableHeight(placeable);
   return { x: x, y: y, w: w, h: h };
 };
 
-export const getPlaceableWidth = function (placeable: any): number {
+const getPlaceableWidth = function (placeable: any): number {
   let w = placeable.w || placeable.data?.width || placeable.width;
   if (placeable?.object) {
     w = placeable?.object?.w || placeable?.object?.data?.width || placeable?.object?.width || w;
@@ -453,7 +501,7 @@ export const getPlaceableWidth = function (placeable: any): number {
   return w;
 };
 
-export const getPlaceableHeight = function (placeable: any): number {
+const getPlaceableHeight = function (placeable: any): number {
   let h = placeable.h || placeable.data?.height || placeable.height;
   if (placeable?.object) {
     h = placeable?.object?.h || placeable?.object?.data?.height || placeable?.object?.height || h;
@@ -461,21 +509,21 @@ export const getPlaceableHeight = function (placeable: any): number {
   return h;
 };
 
-export const getPlaceableX = function (placeable: any): number {
-  let x = placeable._validPosition?.x || placeable.x || placeable?.data?.x;
-  if (placeable?.object) {
-    x = placeable?.object?.x || placeable?.object?.data?.x || x;
-  }
-  return x;
-};
+// const getPlaceableX = function (placeable: any): number {
+//   let x = placeable._validPosition?.x || placeable.x || placeable?.data?.x;
+//   if (placeable?.object) {
+//     x = placeable?.object?.x || placeable?.object?.data?.x || x;
+//   }
+//   return x;
+// };
 
-export const getPlaceableY = function (placeable: any): number {
-  let y = placeable._validPosition?.y || placeable?.y || placeable?.data?.y;
-  if (placeable?.object) {
-    y = placeable?.object?.y || placeable?.object?.data?.y || placeable?.object?.y || y;
-  }
-  return y;
-};
+// const getPlaceableY = function (placeable: any): number {
+//   let y = placeable._validPosition?.y || placeable?.y || placeable?.data?.y;
+//   if (placeable?.object) {
+//     y = placeable?.object?.y || placeable?.object?.data?.y || placeable?.object?.y || y;
+//   }
+//   return y;
+// };
 
 function distance_between_rect(p1: Token, p2: { x: number; y: number; w: number; h: number }) {
   const x1 = p1.x;
@@ -517,23 +565,6 @@ function distance_between_rect(p1: Token, p2: { x: number; y: number; w: number;
 function distance_between(a: { x: number; y: number }, b: { x: number; y: number }): number {
   return Math.max(new Ray(a, b).distance, new Ray(b, a).distance);
 }
-
-// function distance_between_2(a: { x: number; y: number }, b: { x: number; y: number }): number {
-//   const segmentsRight = [{ ray: new Ray({ x: a.x, y: a.y }, { x: b.x, y: b.y }) }];
-//   //@ts-ignore
-//   const distancesRight = measureDistancesInternal(segmentsRight); // , character, shape
-//   // Sum up the distances
-//   const distRight = distancesRight.reduce((acc, val) => acc + val, 0);
-
-//   const segmentsLeft = [{ ray: new Ray({ x: b.x, y: b.y }, { x: a.x, y: a.y }) }];
-//   //@ts-ignore
-//   const distancesLeft = measureDistancesInternal(segmentsLeft); // , character, shape
-//   // Sum up the distances
-//   const distLeft = distancesLeft.reduce((acc, val) => acc + val, 0);
-
-//   const dist = Math.max(distRight, distLeft);
-//   return dist;
-// }
 
 function grids_between_token_and_placeable(token: Token, b: { x: number; y: number; w: number; h: number }) {
   return Math.floor(distance_between_rect(token, b) / <number>canvas.grid?.size) + 1;
